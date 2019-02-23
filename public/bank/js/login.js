f75165cad579e8ecd27468acb72f94dea1690532
larry <15900546817@163.com>
2019-02-23 21:06:39 +0800

    表单重置功能

--- public/bank/js/login.js
+++ public/bank/js/login.js
@@ -1,6 +1,12 @@
 $(function () {
     // 表单校验
     $('#form').bootstrapValidator({
+        // 配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
         // 字段列表  field,  要先在 input 中配置 name 属性
         fields: {
             // 校验用户名
@@ -43,5 +49,57 @@ $(function () {
                 }
             }
         }
+    });
+
+
+    /*
+    2. 使用 submit 按钮, 会进行表单提交, 此时表单校验插件会立刻进行校验
+      (1) 校验成功, 此时会默认提交, 发生页面跳转,  注册表单校验成功事件, 在事件中阻止默认的跳转提交, 通过ajax提交
+      (2) 校验失败, 自动拦截提交
+    
+     注册表单校验成功事件, 在事件中阻止默认的提交, 通过ajax提交
+    */
+    $('#form').on('success.form.bv', function (e) {
+        // 阻止默认提交
+        e.preventDefault();
+        // console.log('当前默认的表单提交已经被阻止, 我们通过ajax提交');
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            // 表单序列化, 自动将所有配置了 name 属性的 input 值进行拼接, 用于提交  
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+                // console.log(info);
+                if (info.error === 1000) {
+                    // alert('用户名错误');
+                    // 调用插件实例方法, 更新username字段状态成失败状态
+                    // updateStatus( field, status, validator );
+                    // 参数1: 需要更新的字段名称
+                    // 参数2: 需要更新成的状态  VALID 成功  INVALID 失败
+                    // 参数3: 配置校验规则, 将来会用配置的规则的 message 进行提示
+                    $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
+                }
+                if (info.error === 1001) {
+                    // alert('密码错误');
+                    $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+                }
+                if (info.success) {
+                    // 登录成功,跳转首页
+                    location.href = 'index.html';
+                }
+            }
+        })
+    });
+
+    /*
+      3. 表单重置功能
+         reset 按钮, 本身就可以重置内容, 所以此时只需要重置状态即可
+         resetForm(false);  只重置状态
+         resetForm(true);   重置内容和状态
+    */
+    $('[type="reset"]').click(function () {
+        // 重置状态即可
+        $('#form').data('bootstrapValidator').resetForm();
     })
 })
