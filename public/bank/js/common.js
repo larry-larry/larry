7c164ac3d91abb132302c5073d785c5059681c03
larry <15900546817@163.com>
2019-02-23 22:29:06 +0800

    退出模态框完成

---
+++ public/bank/js/common.js
@@ -0,0 +1,52 @@
+// 在发送第一个ajax请求, 开启进度条
+// 在全部的ajax回来, 关闭进度条
+
+// ajax全局事件
+// .ajaxComplete(fn);   每个ajax完成时, 都会调用fn回调函数   (完成: 不管成功还是失败)
+// .ajaxSuccess(fn);    每个ajax只要成功了, 就会调用fn
+// .ajaxError(fn);      每个ajax只要失败了, 就会调用fn
+// .ajaxSend(fn);       每个ajax发送前, 都会调用fn
+
+// .ajaxStart(fn);      在第一个ajax开始发送时, 调用fn
+// .ajaxStop(fn);       在全部的ajax完成时, 调用fn  (不管成功还是失败)
+$(document).ajaxStart(function () {
+    // 在第一个ajax开始发送,开启进度条
+    NProgress.start();
+})
+
+// 在全部的ajax完成时, 关闭进度条
+$(document).ajaxStop(function () {
+    // 模拟网络延迟
+    setTimeout(function () {
+        // 结束进度条
+        NProgress.done();
+    }, 500);
+});
+
+// 公用的功能:
+// 1. 左侧二级菜单的切换
+// 2. 左侧整体菜单的切换
+// 3. 公共的退出功能 
+
+// 等待 dom 结构加载完成后, 才会执行
+$(function () {
+    // 点击分类管理按钮,让它的兄弟元素显示出来
+    $('.category').click(function () {
+        $(this).siblings().stop().slideToggle();
+    })
+
+    // 2. 左侧整体菜单的切换
+    $('.icon_menu').click(function () {
+        // 让左侧整个菜单切换显示, 改左侧菜单的 left 值,切换类名就可以
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+    });
+
+    // 3. 退出功能
+    //    点击菜单的退出按钮, 显示一个模态框, 询问用户
+    $('.lt_topbar .icon_logout').click(function () {
+        // 让模态框显示, modal('show')
+        $('#logoutModal').modal('show');
+    })
+})
