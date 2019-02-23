08dea8131e2d4ebedc716c107927ce86aadc9e22
larry <15900546817@163.com>
2019-02-23 20:39:55 +0800

    表单校验完成

--- public/bank/js/login.js
+++ public/bank/js/login.js
@@ -5,9 +5,43 @@ $(function () {
         fields: {
             // 校验用户名
             username: {
-
+                // 校验规则
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        // 提示信息
+                        message: '用户名不能为空'
+                    },
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: '用户名长度为2-6位'
+                    },
+                    // callback 专门用于配置回调提示消息
+                    callback: {
+                        message: '用户名不存在'
+                    }
+                }
             },
-            password: {}
+            password: {
+                // 校验规则
+                validators: {
+                    // 非空
+                    notEmpty: {
+                        // 提示信息
+                        message: '密码不能为空'
+                    },
+                    stringLength: {
+                        min: 2,
+                        max: 6,
+                        message: '密码长度必须是6-12位'
+                    },
+                    // callback 专门用于配置回调提示消息
+                    callback: {
+                        message: '密码错误'
+                    }
+                }
+            }
         }
     })
 })
