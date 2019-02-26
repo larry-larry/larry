993186591a91f296d991439c6ba562baa02eeada
larry <15900546817@163.com>
2019-02-26 19:53:21 +0800

    用户管理页面完成

--- public/bank/js/user.js
+++ public/bank/js/user.js
@@ -2,6 +2,12 @@ $(function () {
 
     var currentPage = 1;   //当前页
     var pageSize = 5;      //每页条数
+
+    // 全局声明id,表示正在编辑的用户
+    var currentId;
+    // 标记修改用户成什么状态
+    var isDelete;
+
     // 进入页面需要渲染
     render();
 
@@ -42,4 +48,38 @@ $(function () {
             }
         })
     }
+
+    // 利用事件委托注册点击事件,让模态框显示出来,因为是动态渲染的数据,所以需要事件委托注册
+    $('tbody').on('click', '.btn', function () {
+        $('#userModal').modal('show');
+
+        // 点击的时候获取点击的是哪一个用户,获取到点击元素身上的id
+        currentId = $(this).parent().data('id');
+
+        // 获取当前用户的状态
+        isDelete = $(this).hasClass('btn-danger') ? 0 : 1;
+    })
+
+    // 点击模态框里的确定按钮发送ajax请求
+    $('#confirmBtn').click(function () {
+
+        $.ajax({
+            type: 'post',
+            url: '/user/updateUser',
+            data: {
+                id: currentId,
+                isDelete: isDelete
+            },
+            dataType: 'json',
+            success: function (info) {
+                console.log(info);
+                // 如果成功关闭模态框
+                if (info.success) {
+                    $('#userModal').modal('hide');
+                    // 重新渲染页面
+                    render();
+                }
+            }
+        })
+    })
 })
